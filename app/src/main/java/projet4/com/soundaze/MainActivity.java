1285b3dd31f14e34266ea5ef23d38a1394fdc9e1
SamirM <samir.marini@student.uclouvain.be>
2019-03-10 19:01:40 +0100

    Intégration du rognage (premier jet)

--- app/src/main/java/projet4/com/soundaze/MainActivity.java
+++ app/src/main/java/projet4/com/soundaze/MainActivity.java
@@ -1,16 +1,84 @@
 package projet4.com.soundaze;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int ADD_AUDIO = 1001;
+    private static final int REQUEST_ID_PERMISSIONS = 1;
+    private ImageView btnAudioTrim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAudioTrim = findViewById(R.id.btn_load);
+        btnAudioTrim.setOnClickListener(this);
+    }
+
+    //Clique sur le bouton "crop"
+    @Override
+    public void onClick(View view) {
+        if (view == btnAudioTrim) {
+            //check storage permission before start trimming
+            if (checkStoragePermission()) {
+                startActivityForResult(new Intent(MainActivity.this, AudioTrimmerActivity.class), ADD_AUDIO); //écran suivant
+                overridePendingTransition(0, 0);
+            } else {
+                requestStoragePermission();
+            }
+        }
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.RECORD_AUDIO},
+                REQUEST_ID_PERMISSIONS);
+    }
+
+    private boolean checkStoragePermission() {
+        return (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_ID_PERMISSIONS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Permission granted, Click again", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ADD_AUDIO) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    //audio trim result will be saved at below path
+                    String path = data.getExtras().getString("INTENT_AUDIO_FILE");
+                    Toast.makeText(MainActivity.this, "Audio stored at " + path, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 
     public void onClickWorkspace(View view)
